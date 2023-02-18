0ed0f68f008c794ef0968218c9ec4afaf0605857
mvatanasov18 <54852701+mvatanasov18@users.noreply.github.com>
2023-02-18 12:53:28 +0200

    Fix star identification

--- src/routes/uploadPhoto/+page.server.ts
+++ src/routes/uploadPhoto/+page.server.ts
@@ -17,35 +17,6 @@ export const load: PageServerLoad = async ({ locals }) => {
 
 
 
-async function getStardataFromLink(link:string) {
-	if (process.env.NOVA_KEY) {
-		const sessionKey = await fetchAstrometrySessionKey(process.env.NOVA_KEY);
-		const data = await submitURL(sessionKey, link);
-		console.log(data);
-		const jobResult = await getJobResults(data)
-
-		if (jobResult.status === "success") {
-			console.log(jobResult.tags)
-			if (jobResult.tags.length) {
-				//post to 3d map
-				console.log("found stars");
-
-				const dataFor3dMap = await fetchData(jobResult.tags);
-				const maybeJson=JSON.stringify( dataFor3dMap)
-				console.log(maybeJson);
-				await fetch(`http://localhost:5173/atlas/api/findStars?dataMap={${maybeJson}},{method:'POST'}`);
-
-			} else {
-				//no stars found
-				fail(500, { error: false, noStars: true })
-			}
-		} else {
-			//display error 
-			//ask the user to send the image again
-			fail(500, { error: true, noStars: true })
-		}
-	}
-}
 
 async function fetchData(starNames: object) {
 
@@ -65,6 +36,10 @@ async function setStarNameAndCoordinates(starName:string,formData:string) {
 	if (starName.startsWith("The star")) {
 		starName = starName.slice("The star".length).trim();
 	}
+	let index=starName.indexOf("(")
+	if(index!==-1){
+		starName = starName.slice(0,index)
+	}
 	starName = starName.replace(/ /g, "+");
 	const res = await fetch("https://cds.unistra.fr/cgi-bin/nph-sesame?" + starName);
 	starName = starName.replace("+", " ");
@@ -72,7 +47,7 @@ async function setStarNameAndCoordinates(starName:string,formData:string) {
 	const row=getDataRow(text)	
 	
 	
-	return `${starName}:${row}`
+	return `${starName}:${row};`
 }
 
 
@@ -89,60 +64,73 @@ function getDataRow(text: string): string {
 	return "";
 }
 
+async function getStardataFromLink(link:string) {
+	if (process.env.NOVA_KEY) {		
+		//get session
+		const sessionKey = await fetchAstrometrySessionKey(process.env.NOVA_KEY);
+		const data = await postPhotoToAstrometry(sessionKey, link);
+		console.log(data);
+		const jobResult = await getJobResults(data)
 
-async function saveURL(url: string) {
-	const connection = await mssqlConnection();
-	const selectRequest = new Request(connection);
-	const selectResult = await selectRequest
-		.input('Username', username)
-		.query('SELECT Id FROM Users WHERE Username = @Username');
-	const id = selectResult.recordset[0].Id;
-	console.log(id);
-	const insertRequest = new Request(connection);
-	const insertResult = await insertRequest
-		.input('Id', uuidv4())
-		.input('UserId', id)
-		.input('ImageURL', url)
-		.query('INSERT INTO UsersImages(Id, UserId, ImageURL) VALUES(@Id, @UserId, @ImageURL)');
-	return insertResult === undefined;
-}
+		if (jobResult.status === "success") {
+			console.log("jobres.tags: ");
+			console.log(jobResult.tags)
 
-async function uploadToUmgur(file: File) {
-	const buffer = Buffer.from(await file.arrayBuffer());
-	const base64 = buffer.toString('base64');
-	let apiUrl = 'https://api.imgur.com/3/image';
-	const formData= new FormData();
-	formData.append("image",base64);
-	let link="";
-	const responce =await fetch(apiUrl,{
-		method:"post",
-		headers:{
-			Authorization: "Client-ID "+process.env.CLIENT_ID
-		},
-		body:formData
-	}).then(data => data.json()).then(data=>{
-		console.log(data.data.link)
-		link =data.data.link;
-	})
-	return link;
-}
+			if (jobResult.tags.length) {
+				//post to 3d map
+				console.log("found stars");
+				
+				
+				const dataFor3dMap = await fetchData(jobResult.tags);
+				console.log(dataFor3dMap);
+				const maybeJson=JSON.stringify( dataFor3dMap)
+				console.log(maybeJson);
+				await fetch(`http://localhost:5173/atlas/api/findStars?dataMap={${maybeJson}},{method:'POST'}`);
 
-async function fetchAstrometrySessionKey(apikey: string) {
-	const response = await fetch('http://nova.astrometry.net/api/login', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded'
-		},
-		body: `request-json=${encodeURIComponent(JSON.stringify({ apikey }))}`
-	});
-	const data = await response.json();
-	if (data.status !== 'success') {
-		console.log('Could not obtain session key: ' + data.message);
+			} else {
+				//no stars found
+				fail(500, { error: false, noStars: true })
+			}
+		} else {
+			//display error 
+			//ask the user to send the image again
+			fail(500, { error: true, noStars: true })
+		}
 	}
-	return data.session;
 }
 
-async function submitURL(sessionId: string, url: string) {
+const upload: Action = async ({ request }) => {
+	try {
+		if (request.method === 'POST') {
+			const form = await request.formData();
+			const image = form.get('img') as File;
+			console.log(form.get('img'));
+			if (image) {
+				console.log(`Received file with name: ${image.name}`);
+				const link = (await uploadToUmgur(image));
+				console.log(link);
+				if (await saveURL(link)) {
+					//should go to the 3d map					
+					await getStardataFromLink(link);
+					return link;
+				} else {
+					fail(500, { error: true, noStars: false });
+				}
+			}
+		}
+	} catch (e) {
+		console.log(e);
+	}
+};
+
+export const actions: Actions = { upload };
+
+async function getJobResults(jobid: string) {
+	const response = await fetch(`https://nova.astrometry.net/api/jobs/${jobid}/info/`);
+	return await response.json();
+}
+
+async function postPhotoToAstrometry(sessionId: string, url: string) {
 	const options = {
 		method: 'POST',
 		body: new URLSearchParams({
@@ -173,34 +161,56 @@ async function submitURL(sessionId: string, url: string) {
 	}
 }
 
-async function getJobResults(jobid: string) {
-	const response = await fetch(`https://nova.astrometry.net/api/jobs/${jobid}/info/`);
-	return await response.json();
+
+async function uploadToUmgur(file: File) {
+	const buffer = Buffer.from(await file.arrayBuffer());
+	const base64 = buffer.toString('base64');
+	let apiUrl = 'https://api.imgur.com/3/image';
+	const formData= new FormData();
+	formData.append("image",base64);
+	let link="";
+	await fetch(apiUrl,{
+		method:"post",
+		headers:{
+			Authorization: "Client-ID "+process.env.CLIENT_ID
+		},
+		body:formData
+	}).then(data => data.json()).then(data=>{
+		link =data.data.link;
+	})
+	return link;
 }
 
-const upload: Action = async ({ request }) => {
-	try {
-		if (request.method === 'POST') {
-			const form = await request.formData();
-			const image = form.get('img') as File;
-			console.log(form.get('img'));
-			if (image) {
-				console.log(`Received file with name: ${image.name}`);
-				await uploadToUmgur(image);
-				const link = (await uploadToUmgur(image));
-				console.log(link);
-				if (await saveURL(link)) {
-					//should go to the 3d map
-					await getStardataFromLink(link);
-					return link;
-				} else {
-					fail(500, { error: true, noStars: false });
-				}
-			}
-		}
-	} catch (e) {
-		console.log(e);
-	}
-};
+async function saveURL(url: string) {
 
-export const actions: Actions = { upload };
+	const connection = await mssqlConnection();
+	const selectRequest = new Request(connection);
+	const selectResult = await selectRequest
+		.input('Username', username)
+		.query('SELECT Id FROM Users WHERE Username = @Username');
+	const id = selectResult.recordset[0].Id;
+	console.log(id);
+	const insertRequest = new Request(connection);
+	const insertResult = await insertRequest
+		.input('Id', uuidv4())
+		.input('UserId', id)
+		.input('ImageURL', url)
+		.query('INSERT INTO UsersImages(Id, UserId, ImageURL) VALUES(@Id, @UserId, @ImageURL)');		
+	return insertResult.recordset === undefined;
+}
+
+
+async function fetchAstrometrySessionKey(apikey: string) {
+	const response = await fetch('http://nova.astrometry.net/api/login', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		body: `request-json=${encodeURIComponent(JSON.stringify({ apikey }))}`
+	});
+	const data = await response.json();
+	if (data.status !== 'success') {
+		console.log('Could not obtain session key: ' + data.message);
+	}
+	return data.session;
+}
